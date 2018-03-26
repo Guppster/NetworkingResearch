edcb019a6961a0a9b986c1a3ae10cc08023d0add
Gurpreet Singh <gurpreet@mailfence.com>
2018-03-26 17:06:29 -0400

    Attempted tierion based hash storage

---
+++ databases/ResearchProject/project/tierionAttempt/examples.sh
@@ -0,0 +1,3 @@
+node mongoHash.js -u localhost:27016/sample -U 'gurpreet@mailfence.com' -P 'pass' -V
+node mongoHash.js -u localhost:27017/sample -U 'gurpreet@mailfence.com' -P 'pass' -c Sakila_films -q '{"Rating":"G"}' -p {}  -V
+node mongoHash.js -u localhost:27017/sample -U 'gurpreet@mailfence.com' -P 'pass' -c Sakila_films -q '{"Rating":"G"}' -p {}
